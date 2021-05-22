bd72690adf9243e28c96d30339bec1db30b23ed9
ComeOosterhof <83632647+ComeOosterhof@users.noreply.github.com>
2021-05-22 19:49:55 +0200

    Création de l'administration et de la classe Communaute

--- communitymanager/models.py
+++ communitymanager/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Communaute(models.Model):
+    nom = models.CharField(max_length=30)
+    abonnes = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.nom
