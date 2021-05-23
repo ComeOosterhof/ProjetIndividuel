4a5e7d387c08b3672d1e93b613625d5f394d05d8
ComeOosterhof <83632647+ComeOosterhof@users.noreply.github.com>
2021-05-23 08:16:31 +0200

    Affichage liste communautés

--- communitymanager/views.py
+++ communitymanager/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from datetime import datetime
 
 # Create your views here.
+from communitymanager.models import Communaute
+
+
+def communautes(request):
+    communautes = Communaute.objects.all()
+    
+    return render(request, 'communautes.html', locals())
