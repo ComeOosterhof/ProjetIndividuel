044f97c830b27d2c6f109aa398e2eaaad6a61515
ComeOosterhof <83632647+ComeOosterhof@users.noreply.github.com>
2021-05-24 19:13:43 +0200

    Formulaire de création d'un post

--- communitymanager/urls.py
+++ communitymanager/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('communautes', views.communautes, name='communautes'),
     path('communaute/<id>', views.communaute, name='communaute'),
-    path('post/<id>', views.post, name='post')
+    path('post/<id>', views.post, name='post'),
+    path('nouveau_post', views.nouveau_post, name='nouveau_post')
 ]
--- communitymanager/views.py
+++ communitymanager/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from datetime import datetime
 
 # Create your views here.
+from communitymanager.forms import PostForm
 from communitymanager.models import Communaute, Post, Commentaire
 
 
@@ -32,3 +33,25 @@ def post(request, id):
     commentaires = Commentaire.objects.filter(post=post)
 
     return render(request, 'communitymanager/post.html', locals())
+
+
+def nouveau_post(request):
+    form = PostForm(request.POST or None)
+
+    if form.is_valid():
+        titre = form.cleaned_data['titre']
+        priorite = form.cleaned_data['priorite']
+        description = form.cleaned_data['description']
+        auteur = form.cleaned_data['auteur']
+        evenementiel = form.cleaned_data['evenementiel']
+        date_creation = form.cleaned_data['date_creation']
+        date_evenement = form.cleaned_data['date_evenement']
+        communaute = form.cleaned_data['communaute']
+
+        # envoi = True
+
+        post = form.save(commit=True)
+
+        return redirect('post', post.id)
+
+    return render(request, 'communitymanager/nouveau_post.html', locals())
